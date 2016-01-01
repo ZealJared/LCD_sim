b9c59b78c5a5c450b444f34db7e405ec81e8948f
Zeal Mayfield <zeal.j.mayfield@gmail.com>
2016-01-01 13:23:54 -0800

    Completed simple character designer.

--- public_html/charDesign.js
+++ public_html/charDesign.js
@@ -1,4 +1,16 @@
 var char = {};
 char.init = function(){
+    char.hex = $("<div>0</div>");
+    char.el = $("<div class='char'></div>");
+    $("body").append(char.el);
+    $("body").append(char.hex);
     // string of 40 0's that become 1's on click, showing hex value
+    for(var i = 0; i < 40; i++){
+        var pix = $("<span>□</span>");
+        char.el.append(pix);
+    }
+    $(".char > span").on("click", function(){
+        $(this).text($(this).text() === "□" ? "■" : "□");
+        char.hex.text(parseInt($(this).parent().text().replace(/□/g, "0").replace(/■/g, "1"), 2).toString(16));
+    });
 };
